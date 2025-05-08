7fe61dc3a2bf7d35ae0184ee8023cb40437391ff
Simon Jäger <simonjager@hotmail.com>
2025-05-08 11:27:45 +0200

    finished emojize problem

--- Lecture 4 - Libraries/problem_sets_4/emojize.py
+++ Lecture 4 - Libraries/problem_sets_4/emojize.py
@@ -1,3 +1,12 @@
 ########## EMOJIZE ##########
 # Imports
 import emoji
+
+#
+user_input = emoji.emojize(input("Input: "), language="alias")
+if emoji.emoji_count(user_input) >= 1:
+    print(f"Output: {user_input}")
+
+# else statement to check if input without emojis will fail, not actually needed, just for my own giggles
+# else:
+#     print(emoji.emojize("No emojis in input :disappointed_face:"))
