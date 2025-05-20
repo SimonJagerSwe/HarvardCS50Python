2d4cf79fdb0c72452538bb3772c106d86e3eaf24
Simon Jäger <simonjager@hotmail.com>
2025-05-20 11:17:20 +0200

    Alternate version, seems to work better with functional process

--- Lecture 4 - Libraries/problem_sets_4/professor.py
+++ Lecture 4 - Libraries/problem_sets_4/professor.py
@@ -1,10 +1,16 @@
 import random
 
-
 def main():
-    get_level()
     guess_counter = 3
     answer = "" 
+    get_level()
+    generate_integer(get_level)
+    print("Problems generated")
+    '''temp = generate_integer(level)
+    print(temp)
+    temp_2 = temp(get_level)
+    print(temp_2)'''
+    
     
 
 
@@ -22,13 +28,15 @@ def get_level():
         except ValueError:
             continue
 
-        generate_integer(level)
+        return level
+        # generate_integer(level)
         break
     
 
 def generate_integer(level):
     print("Initialising integer generation")
     int_list = []
+    problem_list = []
     
     for int in range(10):        
         if level == "1":
@@ -44,9 +52,21 @@ def generate_integer(level):
             int_list.append(integer)
             # print(integer)
      
-    # print(integer)
-        
+    # print(integer)        
     print(int_list)
+
+    for i in range(10):
+        x = random.choice(int_list)
+        print(x)
+        y = random.choice(int_list)
+        print(y)
+        problem = x + y
+        print(problem)
+        problem_list.append([x, y, problem])
+        # return problem
+    
+    print(problem_list)
+    return problem_list
 
 if __name__ == "__main__":
     main()
