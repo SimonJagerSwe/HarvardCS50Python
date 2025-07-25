8d908659efa7ebe528cef0ebcacf8bf8ea40c10f
Simon Jäger <simonjager@hotmail.com>
2025-07-25 21:50:24 +0200

    works

--- Lecture 7 - Regular Expressions/problem_sets_7/um_2.py
+++ Lecture 7 - Regular Expressions/problem_sets_7/um_2.py
@@ -1,21 +1,23 @@
 import re
 
 def main():
-    print(count(input("Text: ").lower()))
+    print(count(input("Text: ")))
 
 
 def count(s):
-    counter = 0
+    # counter = 0
     # delimiters = [",", "."]
-    new_s = re.sub(r"[ |,|.]", " ", s)
+    new_s = re.sub(r"[,|.|?|!|;|\s+]", " ", s).lower()
     # return new_s
     split_s = new_s.split(" ")
+    um_list = []    
+    # print(split_s)
     # return split_s
     for word in split_s:
         if word == "um":
-            counter += 1
+            um_list.append(word)
 
-    return counter    
+    return len(um_list)
 
 if __name__ == "__main__":
     main()
