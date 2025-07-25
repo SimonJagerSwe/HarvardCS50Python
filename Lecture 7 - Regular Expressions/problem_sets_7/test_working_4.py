e379afbf087c2c633ef9ec4ce9644ed454e237fa
Simon Jäger <simonjager@hotmail.com>
2025-07-25 17:43:47 +0200

    new tests, better coverage

--- Lecture 7 - Regular Expressions/problem_sets_7/test_working_4.py
+++ Lecture 7 - Regular Expressions/problem_sets_7/test_working_4.py
@@ -1,24 +1,33 @@
 from working_4 import time_conversion
 
 
-def test_valid_am_to_am():
-    assert time_conversion("1 AM TO 9 AM") == "01:00 to 09:00"
-    assert time_conversion("1:32 AM TO 8:45 AM") == "01:32 to 08:45"
+'''def main():
+    test_am_to_pm()
+    test_am_to_am()
+    # test_pm_to_am()
+    # test_pm_to_pm()
+    # test_value_errors()
+'''
 
-def test_valid_pm_to_pm():
-    assert time_conversion("12 PM TO 9 PM") == "12:00 to 21:00"
-    assert time_conversion("12:01 PM TO 9:32 PM") == "12:01 to 21:32"
-    
-def test_valid_am_to_pm():
-    assert time_conversion("9 AM TO 5 PM") == "09:00 to 17:00"
-    assert time_conversion("3:15 AM TO 11:45 PM") == "03:15 to 23:45"
+def test_am_to_pm():
+    assert time_conversion("9 AM TO 9 PM") == "09:00 to 21:00"
+    assert time_conversion("7:00 AM TO 3:00 PM") == "07:00 to 15:00"
+    assert time_conversion("1:32 AM TO 8:45 PM") == "01:32 to 20:45"
 
-def test_valid_pm_to_am():
-    assert time_conversion("2 PM TO 1 AM") == "14:00 to 01:00"
-    assert time_conversion("7:34 PM TO 12:34 AM") == "19:34 to 00:34"
+def test_am_to_am():
+    assert time_conversion("1 AM TO 11 AM") == "01:00 to 11:00"
+    assert time_conversion("3:00 AM TO 8:00 AM") == "03:00 to 08:00"
+    assert time_conversion("12:30 AM TO 8:50 AM") == "00:30 to 08:50"
 
-def test_valid__12_to_12():
-    assert time_conversion("12 AM TO 12 PM") == "00:00 to 12:00"
-    assert time_conversion("12 AM TO 12 AM") == "00:00 to 00:00"
-    assert time_conversion("12 PM TO 12 AM") == "12:00 to 00:00"
-    assert time_conversion("12 PM TO 12 PM") == "12:00 to 12:00"
+def test_pm_to_am():
+    assert time_conversion("9 PM TO 6 AM") == "21:00 to 06:00"
+    assert time_conversion("7:00 PM TO 2:00 AM") == "19:00 to 02:00"
+    assert time_conversion("8:45 PM TO 5:36 AM") == "20:45 to 05:36"
+
+def test_pm_to_pm():
+    assert time_conversion("1 PM TO 9 PM") == "13:00 to 21:00"
+    assert time_conversion("2:00 PM TO 11:00 PM") == "14:00 to 23:00"
+    assert time_conversion("4:23 PM TO 10:32 PM") == "16:23 to 22:32"
+
+'''if __name__ == "__main__":
+    main()'''
