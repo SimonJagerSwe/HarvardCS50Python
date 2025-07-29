c7b9fb7bef9816e7b05a22eabce662af7e7af819
Simon Jäger <simonjager@hotmail.com>
2025-07-29 19:37:27 +0200

    Using decorators

--- Lecture 8 - Object Oriented Programming/student_4.py
+++ Lecture 8 - Object Oriented Programming/student_4.py
@@ -3,10 +3,10 @@
 # Example 1, using decorators to code defensively
 class Student:
     def __init__(self, name, house):
-        if not name:
-            raise ValueError("Missing name")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
+        # if not name:
+        #     raise ValueError("Missing name")
+        # if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+        #     raise ValueError("Invalid house")
         self.name = name
         self.house = house
 
@@ -14,16 +14,33 @@ class Student:
         return f"{self.name} from {self.house}"
     
     # Getter
-    def house(self):
-        return self.house
+    @property
+    def name(self):
+        return self._name
     
     # Setter
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+    
+    # Getter
+    @property
+    def house(self):
+        return self._house
+    
+    # Setter
+    @house.setter
     def house(self, house):
-        self.house = house
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
     
 
 def main():
     student = get_student()
+    # student.house = "Number Four, Privet Drive"
     print(student)
 
 def get_student():
