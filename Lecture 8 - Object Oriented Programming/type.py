fa1664fbcf912ca318a452292f3452b81387f71f
Simon Jäger <simonjager@hotmail.com>
2025-07-30 10:22:23 +0200

    Showing how to find the type of a variable

---
+++ Lecture 8 - Object Oriented Programming/type.py
@@ -0,0 +1,9 @@
+########## TYPE ##########
+
+# Example 1, finding a variable class type
+print(type(50))
+print(type("Hello, World!"))
+print(type([]))
+print(type(list()))
+print(type({}))
+print(type(dict()))
