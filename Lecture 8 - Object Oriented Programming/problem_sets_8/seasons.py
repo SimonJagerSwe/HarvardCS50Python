d3b14ded670901c2f8d90af052625cef970bf7f4
Simon Jäger <simonjager@hotmail.com>
2025-08-05 20:52:10 +0200

    added month and date length checking to validation

--- Lecture 8 - Object Oriented Programming/problem_sets_8/seasons.py
+++ Lecture 8 - Object Oriented Programming/problem_sets_8/seasons.py
@@ -1,11 +1,13 @@
 # Imports
+import datetime
 import sys
-from datetime import date
+# from datetime import date
 
 
 def main():
     birth_date = (validate_date(input("Date of birth: ")))
     print(birth_date)
+    print(convert_date(birth_date))
 
 
 def validate_date(birth_date):
@@ -15,7 +17,11 @@ def validate_date(birth_date):
     if (
         len(year) != 4 or
         len(month) != 2 or
-        len(day) != 2
+        int(month) < 1 or
+        int(month) > 12 or
+        len(day) != 2 or
+        int(day) < 1 or
+        int(day) > 31
     ):
         sys.exit("Invalid date format")
 
@@ -25,7 +31,8 @@ def validate_date(birth_date):
         return f"{year}-{month}-{day}"
     
 
-
+def convert_date(birth_str):
+    return datetime.date.today()
 
 if __name__ == "__main__":
     main()
