77287e09d56d680862fa2083aaa9f1f8b90727cb
Simon Jäger <simonjager@hotmail.com>
2025-08-05 19:54:27 +0200

    date validation works

--- Lecture 8 - Object Oriented Programming/problem_sets_8/seasons.py
+++ Lecture 8 - Object Oriented Programming/problem_sets_8/seasons.py
@@ -1,12 +1,25 @@
 # Imports
+import sys
 from datetime import date
 
 
 def main():
-    ...
+    print(validate_date(input("Date of birth: ")))
 
 
-...
+def validate_date(date):
+    year = date.split("-")[0]
+    month = date.split("-")[1]
+    day = date.split("-")[2]
+    if (
+        len(year) != 4 or
+        len(month) != 2 or
+        len(day) != 2
+    ):
+        sys.exit("Invalid date format")
+
+    else:
+        return f"{year}-{month}-{day}"
 
 
 if __name__ == "__main__":
