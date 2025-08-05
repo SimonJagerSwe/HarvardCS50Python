624e7ea7963cdfef948196e5a63178cb489d63dd
Simon Jäger <simonjager@hotmail.com>
2025-08-05 21:06:43 +0200

    will create new file to try other checking method

--- Lecture 8 - Object Oriented Programming/problem_sets_8/seasons.py
+++ Lecture 8 - Object Oriented Programming/problem_sets_8/seasons.py
@@ -1,7 +1,7 @@
 # Imports
-import datetime
+# import datetime
 import sys
-# from datetime import date
+from datetime import date, datetime
 
 
 def main():
@@ -11,28 +11,40 @@ def main():
 
 
 def validate_date(birth_date):
-    year = birth_date.split("-")[0]
-    month = birth_date.split("-")[1]
-    day = birth_date.split("-")[2]
+    # current_date = datetime.date.today()
+    # print(str(current_date))
+    # print(type(current_date))
+    current_year = str(date.today()).split("-")[0]
+    current_month = str(date.today()).split("-")[1]
+    current_day = str(date.today()).split("-")[2]
+    # print(current_year)
+    # print(current_month)
+    # print(current_day)
+    given_year = birth_date.split("-")[0]
+    given_month = birth_date.split("-")[1]
+    given_day = birth_date.split("-")[2]
     if (
-        len(year) != 4 or
-        len(month) != 2 or
-        int(month) < 1 or
-        int(month) > 12 or
-        len(day) != 2 or
-        int(day) < 1 or
-        int(day) > 31
+        len(given_year) != 4 or
+        int(given_year) > int(current_year) or
+        len(given_month) != 2 or
+        int(given_month) < 1 or
+        int(given_month) > 12 or
+        len(given_day) != 2 or
+        int(given_day) < 1 or
+        int(given_day) > 31
     ):
         sys.exit("Invalid date format")
 
     else:
         # birth_date = f"{year}-{month}-{day}"
         # return birth_date
-        return f"{year}-{month}-{day}"
+        return f"{given_year}-{given_month}-{given_day}"
     
 
 def convert_date(birth_str):
-    return datetime.date.today()
+    # datetime.date.today()
+    # return
+    pass
 
 if __name__ == "__main__":
     main()
