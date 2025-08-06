8b5a626fd42e234c1c632bd06151c2d54024f0b5
Simon Jäger <simonjager@hotmail.com>
2025-08-06 16:17:25 +0200

    seems to run correctly

--- Lecture 8 - Object Oriented Programming/problem_sets_8/shirtificate.py
+++ Lecture 8 - Object Oriented Programming/problem_sets_8/shirtificate.py
@@ -1,20 +1,21 @@
 # Imports
 from fpdf import FPDF
-from PIL import Image, ImageOps
 
 def main():
-    shirt = "shirtificate.png"
-    shirt_printer("CS50 Shirtificate", input("Name: "), shirt)
+    shirt_printer("CS50 Shirtificate", input("Name: "), "shirtificate.png")
 
 
 def shirt_printer(header, name, shirt):
-    pdf = FPDF(orientation="P", unit="mm", format=(210, 297))
+    pdf = FPDF(orientation="P", format="A4")
     pdf.add_page()
-    pdf.set_font("helvetica", size=24)
-    pdf.image(shirt, 17, 67, 175)    
-    pdf.cell(75, 50, name, align="C")
-    pdf.cell(200, 200, header, align="C")    
-    pdf.output("empty_test.pdf")
+    pdf.set_font("helvetica", "B", size=50)
+    pdf.cell(0, 60, header, new_x="LMARGIN", new_y="NEXT", align="C")
+    pdf.image(shirt, w=pdf.epw)
+    pdf.set_font_size(30)
+    pdf.set_text_color(255, 255, 255)
+    pdf.text(x=47.5, y=140, text=f"{name} took CS50")
+    pdf.output("shirtificate.pdf")
+
 
 if __name__ == "__main__":
     main()
