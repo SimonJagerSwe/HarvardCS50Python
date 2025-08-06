80cf65ed8feca10e3f6172f64b3c366f00a491e3
Simon Jäger <simonjager@hotmail.com>
2025-08-06 12:58:36 +0200

    Tentative setup of file

---
+++ Lecture 8 - Object Oriented Programming/problem_sets_8/jar.py
@@ -0,0 +1,29 @@
+class Jar:
+    def __init__(self, capacity=12):
+        self.capacity = capacity
+        self.size = 0
+
+    def __str__(self):
+        ...
+
+    def deposit(self, n):
+        ...
+
+    def withdraw(self, n):
+        ...
+
+    @property
+    def capacity(self):
+        ...
+
+    @property
+    def size(self):
+        ...
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
