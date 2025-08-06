d924a70d0a71b25156b23a2de644df708c688ae4
Simon Jäger <simonjager@hotmail.com>
2025-08-06 15:32:26 +0200

    Need to fix this...

--- Lecture 8 - Object Oriented Programming/problem_sets_8/shirtificate.py
+++ Lecture 8 - Object Oriented Programming/problem_sets_8/shirtificate.py
@@ -4,16 +4,16 @@ from PIL import Image, ImageOps
 
 def main():
     shirt = "shirtificate.png"
-    shirt_printer("CS50P", input("Name: "), shirt)
+    shirt_printer("CS50 Shirtificate", input("Name: "), shirt)
 
 
 def shirt_printer(header, name, shirt):
     pdf = FPDF(orientation="P", unit="mm", format=(210, 297))
     pdf.add_page()
-    pdf.set_font("helvetica", style="B", size=16)
-    pdf.image(shirt, 17, 100, 175)    
-    # pdf.cell()
-    # pdf.cell(50, 50, header, align="C")    
+    pdf.set_font("helvetica", size=24)
+    pdf.image(shirt, 17, 67, 175)    
+    pdf.cell(75, 50, name, align="C")
+    pdf.cell(200, 200, header, align="C")    
     pdf.output("empty_test.pdf")
 
 if __name__ == "__main__":
