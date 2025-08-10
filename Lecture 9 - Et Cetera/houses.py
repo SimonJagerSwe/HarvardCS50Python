54c55da63cdf5eb22359a37fc164bb202271f7da
Simon Jäger <simonjager@hotmail.com>
2025-08-10 14:58:56 +0200

    First two examples

---
+++ Lecture 9 - Et Cetera/houses.py
@@ -0,0 +1,36 @@
+# Example 1, eliminating duplicates in a list
+'''
+students = [
+    {"name" : "Hermione", "house" : "Gryffindor"},
+    {"name" : "Harry", "house" : "Gryffindor"},
+    {"name" : "Ron", "house" : "Gryffindor"},
+    {"name" : "Draco", "house" : "Slytherin"},
+    {"name" : "Padma", "house" : "Ravenclaw"}
+    ]
+
+houses = []
+for student in students:
+    if student["house"] not in houses:
+        houses.append(student["house"])
+
+
+for house in sorted(houses):
+    print(house)
+'''
+
+# Example 2, same, using sets
+students = [
+    {"name" : "Hermione", "house" : "Gryffindor"},
+    {"name" : "Harry", "house" : "Gryffindor"},
+    {"name" : "Ron", "house" : "Gryffindor"},
+    {"name" : "Draco", "house" : "Slytherin"},
+    {"name" : "Padma", "house" : "Ravenclaw"}
+    ]
+
+houses = set()
+for student in students:
+    houses.add(student["house"])
+
+
+for house in sorted(houses):
+    print(house)
