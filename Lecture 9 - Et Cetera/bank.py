75c42556e794f24a68633cbb3ef6d0f930c5f126
Simon Jäger <simonjager@hotmail.com>
2025-08-10 15:14:39 +0200

    Two examples set

---
+++ Lecture 9 - Et Cetera/bank.py
@@ -0,0 +1,53 @@
+# Example 1, using global variable
+'''
+balance = 0
+
+def main():
+    print(f"Balance: {balance}")
+    deposit(100)
+    withdraw(50)
+    print(f"Balance: {balance}")
+
+
+def deposit(n):
+    global balance
+    balance += n
+
+def withdraw(n):
+    global balance
+    balance -= n
+
+
+if __name__ == "__main__":
+    main()
+'''
+
+
+# Example 2, using OOP
+class Account:
+    def __init__(self):
+        self._balance = 0
+
+    @property
+    def balance(self):
+        return self._balance
+    
+
+    def deposit(self, n):
+        self._balance += n
+
+
+    def withdraw(self, n):
+        self._balance -= n
+
+
+def main():
+    account = Account()
+    print(f"Balance: {account.balance}")
+    account.deposit(100)
+    account.withdraw(50)
+    print(f"Balance: {account.balance}")
+
+
+if __name__ == "__main__":
+    main()
