e83e79e4c75666e038c00a5e6b9f9b2b901c9e18
Simon Jäger <simonjager@hotmail.com>
2025-08-11 18:47:39 +0200

    Final little funny thing

---
+++ Lecture 9 - Et Cetera/say.py
@@ -0,0 +1,9 @@
+# Example 1, because why the hell not end with a talking cow, right?
+import cowsay
+import pyttsx3
+
+engine = pyttsx3.init()
+this = input("What's this? ")
+cowsay.cow(this)
+engine.say(this)
+engine.runAndWait()
